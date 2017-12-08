f9c2317eb24ec719a1dee979b1d9237651f4e9b0
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2017-12-08 12:58:01 +0100

    Add script for building TPC-H monetdb databases

---
+++ tpch_build.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# The path to the database farm
+farm_path=
+
+# The TPC-H scale factor 
+scale_factor=
+
+usage() {
+    echo "Usage: $0 --sf <scale factor> --farm <farm path>"
+}
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -s|--sf)
+            scale_factor=$2
+            shift
+            shift
+            ;;
+        -f|--farm)
+            farm_path=$2
+            shift
+            shift
+            ;;
+        *)
+            echo "$0: Unknown parameter $1"
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$scale_factor" -o -z "$farm_path" ]; then
+    usage
+    exit 1
+fi
+
+# Add dot monetdb file for permissions
+cat << EOF > $HOME/.monetdb
+user=monetdb
+password=monetdb
+save_history=true
+EOF
+
+pushd 01_build/dbgen
+make
+# Create the data for the scale factor
+./dbgen -vf -s "$scale_factor"
+
+mkdir -p ../../02_load/SF-"$scale_factor"/data
+mv *.tbl ../../02_load/SF-"$scale_factor"/data
+popd
+pushd 02_load
+
+# Create the database
+if [ ! -e "$farm_path" ]; then
+    monetdbd create "$farm_path"
+fi
+
+# Start the daemon
+monetdbd start "$farm_path"
+# Load the data
+./sf_build.sh SF-"$scale_factor"
+# Stop the daemon
+monetdbd stop "$farm_path"
+
+echo "SF-$scale_factor loaded. Use"
+echo "mserver5 --dbpath=$farm_path/SF-$scale_factor --set monet_vault_key=$farm_path/SF-$scale_factor/.vaultkey"
+echo "to start the server."
